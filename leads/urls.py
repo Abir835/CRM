cc7ae80d0a1188e3518b3a67b9caa28e3e58aef4
Abir835 <abirhasan.raj.bd@gmail.com>
2022-01-23 23:17:53 +0600

    <add> url routing lead update

--- leads/urls.py
+++ leads/urls.py
@@ -3,5 +3,7 @@ from leads import views
 urlpatterns = [
     path('', views.lead_list, name='lead_list'),
     path('lead_details/<int:pk>/', views.lead_details, name='lead_details'),
+    path('lead_create/', views.lead_create, name='lead_create'),
+    path('lead_update/<int:pk>/', views.lead_update, name='lead_update'),
 
 ]
