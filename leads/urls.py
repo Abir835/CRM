bfe9f7838639854b56fea748fbedfac5f1a3b618
Abir835 <abirhasan.raj.bd@gmail.com>
2022-01-23 21:45:28 +0600

    <add> url routing lead details

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from leads import views
 urlpatterns = [
-    path('', views.Index, name='index')
+    path('', views.lead_list, name='lead_list'),
+    path('lead_details/<int:pk>/', views.lead_details, name='lead_details'),
+
 ]
--- leads/views.py
+++ leads/views.py
@@ -6,9 +6,17 @@ from leads.models import *
 # Create your views here.
 
 
-def Index(request):
+def lead_list(request):
     leads = Lead.objects.all()
     context = {
         'leads': leads
     }
-    return render(request, 'index.html', context)
+    return render(request, 'leads/lead_list.html', context)
+
+
+def lead_details(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead_details': lead,
+    }
+    return render(request, 'leads/lead_details.html', context)
